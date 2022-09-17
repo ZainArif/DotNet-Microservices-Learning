ddca175679de0c5e5e7631ce06ede1d34632b486
Zain Arif <zainarif14197@gmail.com>
2022-09-17 17:42:11 +0500

    catalog service

---
+++ Play.Catalog/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Service.Dtos;
+
+namespace Play.Catalog.Service.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> items = new()
+        {
+            new ItemDto(1, "Potion", "", 4, DateTimeOffset.UtcNow),
+            new ItemDto(2, "Antidote", "", 7, DateTimeOffset.UtcNow),
+            new ItemDto(3, "Bronze", "", 10, DateTimeOffset.UtcNow)
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ItemDto GetById(int id)
+        {
+            var item = items.Where(item => item.Id == id).SingleOrDefault();
+            return item;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(4, createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(item);
+
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        }
+    }
+}
